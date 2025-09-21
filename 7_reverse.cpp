7fe6cd8032f229dd91fdbdc0bd544383df538452
Faheem Iqbal <zargarfaheem4229@gmail.com>
2025-09-21 11:48:21 +0530

    2 Questions added

---
+++ 7_reverse.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+    int reverse(int x) {
+        int digit,rev=0;
+        while(x!=0){
+            digit=x%10;
+            x=x/10;
+            if (rev > INT_MAX/10 || (rev == INT_MAX/10 && digit > 7)) return 0;
+            if (rev < INT_MIN/10 || (rev == INT_MIN/10 && digit < -8)) return 0;
+            rev=rev*10+digit;
+        }
+           
+        return rev;
+    }
+    
+int main(){
+    int x;
+    cout<<"Enter number"<<endl;
+    cin>>x;
+   cout<<"Reverse number is "<< reverse(x);
+
+}
---
+++ 9_palindrome.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    bool isPalindrome(int x) {
+        if (x < 0) return false; // negative numbers can't be palindrome
+        string s = to_string(x); // copy 
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        return s == rev;
+    }
+int main(){
+    int n;
+    cout<<"Enter Number"<<endl;
+    cin>>n;
+    if(isPalindrome(n))cout<<"IS Palindrome"<<endl;
+    else cout<<"Not palindrome";
+}
