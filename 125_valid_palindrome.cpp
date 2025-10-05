a3d821f7de23ec903b98e53bb4954923a2e32f17
Faheem Iqbal <zargarfaheem4229@gmail.com>
2025-10-05 21:26:28 +0530

    update

--- 125_valid_palindrome.cpp
+++ 125_valid_palindrome.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
     bool isPalindrome(string s) {
         s.erase(remove_if(s.begin(),s.end(),[](char c){
-            return !isalnum(c);    //isalpha(c) keeps only alphabets. isalnum(c) keeps only letters and numbers
+            return !isalnum(c);    //isalpha(c) keeps only alphabets.  isalnum(c) keeps only letters and numbers
              }), s.end());
             
             transform(s.begin(),s.end(),s.begin(), ::tolower);
