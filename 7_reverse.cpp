158719e2fe0b46022475fad36332031429d7d5ec
Faheem Iqbal <zargarfaheem4229@gmail.com>
2025-10-13 22:02:29 +0530

    Update

--- 7_reverse.cpp
+++ 7_reverse.cpp
@@ -6,7 +6,7 @@ using namespace std;
         while(x!=0){
             digit=x%10;
             x=x/10;
-            if (rev > INT_MAX/10 || (rev == INT_MAX/10 && digit > 7)) return 0;
+            if (rev > INT_MAX/10 ||(rev == INT_MAX/10 && digit > 7)) return 0;
             if (rev < INT_MIN/10 || (rev == INT_MIN/10 && digit < -8)) return 0;
             rev=rev*10+digit;
         }  
